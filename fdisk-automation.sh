78d5119d00dbceb03603c68e222116b98c68db8f
Mohsen K. Amini <mohsenkamini@gmail.com>
2020-12-15 15:45:53 +0000

    Update fdisk-automation.sh

--- fdisk-automation.sh
+++ fdisk-automation.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 
+# INPUT FILE SECTION
+## input file contains the info about partiotions
+
+echo 'please enter the input file name'
+read 'inputfile'
+
+nl=$(wc -l < $inputfile )
+((nl=nl-1))                                     # so that it doesn't read the first line
+
+mkdir -p /tmp/fdisk.sh/ 2> /dev/null
+cat $inputfile | tail -n $nl > /tmp/fdisk.sh/inputfile
+input=/tmp/fdisk.sh/inputfile
+
+##
